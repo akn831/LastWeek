5f6232de8cadc78b06ed1c2d13573d6f2f8a40c4
田仲章乃 <>
2019-06-27 10:16:02 +0900

    フェードイン追加

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $(function() {
+    // lightbox
     $(".img").on("click", function() {
         $('<div id="over">').appendTo($(".container")).hide().fadeIn();
 
@@ -20,25 +21,42 @@ $(function() {
             })
         })
     })
-})
 
-new Chart(document.getElementById("myChart"), {
-    type: "doughnut",
-    data: {
-        labels: ["船代", "ポートチャージやビザ等の諸費用", "離脱費用", "お小遣い"],
-        datasets: [
-            {
-                data: [990000,100000,400000,200000],
-                backgroundColor: [
-                    "rgb(255,99,132)",
-                    "rgb(54,162,235)",
-                    "rgb(255,205,86)",
-                    "rgb(234,57,10)",
-                ]
+    // 円グラフ
+    new Chart(document.getElementById("myChart"), {
+        type: "doughnut",
+        data: {
+            labels: ["船代", "ポートチャージやビザ等の諸費用", "離脱費用", "お小遣い"],
+            datasets: [
+                {
+                    data: [990000,100000,400000,200000],
+                    backgroundColor: [
+                        "rgb(255,99,132)",
+                        "rgb(54,162,235)",
+                        "rgb(255,205,86)",
+                        "rgb(234,57,10)",
+                    ]
+                }
+            ]
+        }
+    })
+
+    $("#myChart").css({"width":"600px", "height":"300"});
+
+    // フェードイン
+    $(window).on("scroll", function() {
+        $(".fadein").each(function() {
+            let elemPos = $(this).offset().top;
+            let scroll = $(window).scrollTop();
+            let windowHeight = $(window).height();
+
+            if(scroll > elemPos - windowHeight + 500) {
+                $(this).addClass("scrollin");   
             }
-        ]
-    }
-})
+        })
 
-$("#myChart").css({"width":"600px", "height":"300"});
+
+    
+    })
+})
 
